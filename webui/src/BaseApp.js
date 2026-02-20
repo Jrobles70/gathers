a950c4c19c06c58a6d8f9c417f262854f7108fee
Mihail Morosan <morosanmihail@outlook.com>
2026-02-20 18:21:10 +0000

    Fix home page in search only mode.

--- webui/src/BaseApp.js
+++ webui/src/BaseApp.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { OperationsProvider, ModeProvider } from "./OperationsContext";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import CardListView from "./Views/CardListView";
 import SearchView from "./Views/SearchView";
 
 export default function BaseApp({ mode = "full" }) {
+  const isSearchOnly = mode === "search-only";
   return (
     <ModeProvider mode={mode}>
       <OperationsProvider>
         <BrowserRouter>
           <Routes>
-            <Route exact path="/" element={<CardListView />} />
+            <Route
+              exact
+              path="/"
+              element={
+                isSearchOnly ? <Navigate to="/search" /> : <CardListView />
+              }
+            />
             <Route path="/search" element={<SearchView />} />
             <Route path="/c/:collection" element={<CardListView />} />
             <Route
